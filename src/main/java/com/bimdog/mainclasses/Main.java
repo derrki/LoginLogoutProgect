b0ce1fadaf31d5524e318f5360b681e5b97b8d0f
Roman Kaminskiy - pc <derrki@gmail.com>
2019-03-02 18:48:14 +0200

    Написано код для запису даних в базу даних

--- src/main/java/com/bimdog/mainclasses/Main.java
+++ src/main/java/com/bimdog/mainclasses/Main.java
@@ -3,21 +3,34 @@ package com.bimdog.mainclasses;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 public class Main {
 
-//    private static final String HOST_MYSQL = "jdbc:mysql://localhost:3306/users_db"+
-//            "?verifyServerCertificate=false"+
-//            "&useSSL=false"+
-//            "&requireSSL=false"+
-//            "&useLegacyDatetimeCode=false"+
-//            "&amp"+
-//            "&serverTimezone=UTC";
-//    private static final String USERNAME_MYSQL = "root";
-//    private static final String PASSWORD_MYSQL = "root";
-//
-//    public static void main(String[] args) {
-//
-//
-//    }
+  public static final String HOST_MYSQL = "jdbc:mysql://localhost:3306/users_db"+
+            "?verifyServerCertificate=false"+
+            "&useSSL=false"+
+            "&requireSSL=false"+
+            "&useLegacyDatetimeCode=false"+
+            "&amp"+
+            "&serverTimezone=UTC";
+  public static final String USERNAME_MYSQL = "root";
+  public static final String PASSWORD_MYSQL = "root";
+
+    public static void main(String[] args) {
+
+        try {
+            runTest();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void runTest() throws SQLException {
+        try(Connection connection = DriverManager.getConnection(HOST_MYSQL, USERNAME_MYSQL, PASSWORD_MYSQL);
+            Statement statement = connection.createStatement())
+        {
+            statement.execute("INSERT INTO users(name, surname, login, password) VALUES('Vasa', 'vas', 'log', 'qwerty');");
+        };
+    }
 }
