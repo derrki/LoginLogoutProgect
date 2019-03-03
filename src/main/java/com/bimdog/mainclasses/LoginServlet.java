1241c8bdce8fdae215d2291711715f0e1c88fe40
Roman Kaminskiy - pc <derrki@gmail.com>
2019-03-03 15:21:42 +0200

    аписано сервлет LoginServlet.java котрий приймає введені дані з форми і виводить

---
+++ src/main/java/com/bimdog/mainclasses/LoginServlet.java
@@ -0,0 +1,33 @@
+package com.bimdog.mainclasses;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+@WebServlet("/LoginServlet")
+public class LoginServlet extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html");
+        PrintWriter out = response.getWriter();
+        request.getRequestDispatcher("link.html").include(request, response);
+
+        String name = request.getParameter("name");
+        String surname = request.getParameter("surname");
+        String login = request.getParameter("login");
+        String password = request.getParameter("password");
+
+        if(password.equals("root")){
+            out.print("Welcome, " + name + " " + surname);
+            HttpSession session = request.getSession();
+            session.setAttribute("name", name);
+            session.setAttribute("surname", surname);
+        }
+
+    }
+}
