8570170ebaeb2ae0173bbbaa613d99edb60309fc
Roman Kaminskiy - pc <derrki@gmail.com>
2019-03-03 14:35:09 +0200

    Додано декілька Html файлів, для відображення панелі вводу та виводу результатів роботи

--- src/main/java/com/bimdog/mainclasses/Main.java
+++ src/main/java/com/bimdog/mainclasses/Main.java
@@ -5,49 +5,49 @@ import java.util.LinkedList;
 
 public class Main {
 
-  public static final String HOST_MYSQL = "jdbc:mysql://localhost:3306/users_db"+
-            "?verifyServerCertificate=false"+
-            "&useSSL=false"+
-            "&requireSSL=false"+
-            "&useLegacyDatetimeCode=false"+
-            "&amp"+
-            "&serverTimezone=UTC";
-  public static final String USERNAME_MYSQL = "root";
-  public static final String PASSWORD_MYSQL = "root";
-
-    public static void main(String[] args) throws SQLException {
-
-        //connect
-        String comand = "INSERT INTO users(name, surname, login, password) VALUES('Vasa8', 'vas8', 'log8', 'qwerty8');";
-        DatabaseManager databaseManager = new DatabaseManager();
-        databaseManager.connect(HOST_MYSQL, USERNAME_MYSQL, PASSWORD_MYSQL);
-
-        //insert
-        Statement statement = databaseManager.getConnection().createStatement();
-        databaseManager.insert(statement, comand);
-
-
-        //query
-        String query = "SELECT * from users";
-        ResultSet resultSet = databaseManager.query(statement, query);
-
-        LinkedList<User> listUser = new LinkedList<>();
-        User user;
-
-        while (resultSet.next()){
-
-           String name = resultSet.getString("name");
-           String surname = resultSet.getString("surname");
-           String login = resultSet.getString("login");
-           String password = resultSet.getString("password");
-           String country = "UA";
-           user = new User(name, surname, login, password, country);
-           listUser.add(user);
-        }
-
-        for (User userOne: listUser) {
-
-            System.out.println(userOne);
-        }
-    }
+//  public static final String HOST_MYSQL = "jdbc:mysql://localhost:3306/users_db"+
+//            "?verifyServerCertificate=false"+
+//            "&useSSL=false"+
+//            "&requireSSL=false"+
+//            "&useLegacyDatetimeCode=false"+
+//            "&amp"+
+//            "&serverTimezone=UTC";
+//  public static final String USERNAME_MYSQL = "root";
+//  public static final String PASSWORD_MYSQL = "root";
+//
+//    public static void main(String[] args) throws SQLException {
+//
+//        //connect
+//        String comand = "INSERT INTO users(name, surname, login, password) VALUES('Vasa8', 'vas8', 'log8', 'qwerty8');";
+//        DatabaseManager databaseManager = new DatabaseManager();
+//        databaseManager.connect(HOST_MYSQL, USERNAME_MYSQL, PASSWORD_MYSQL);
+//
+//        //insert
+//        Statement statement = databaseManager.getConnection().createStatement();
+//        databaseManager.insert(statement, comand);
+//
+//
+//        //query
+//        String query = "SELECT * from users";
+//        ResultSet resultSet = databaseManager.query(statement, query);
+//
+//        LinkedList<User> listUser = new LinkedList<>();
+//        User user;
+//
+//        while (resultSet.next()){
+//
+//           String name = resultSet.getString("name");
+//           String surname = resultSet.getString("surname");
+//           String login = resultSet.getString("login");
+//           String password = resultSet.getString("password");
+//           String country = "UA";
+//           user = new User(name, surname, login, password, country);
+//           listUser.add(user);
+//        }
+//
+//        for (User userOne: listUser) {
+//
+//            System.out.println(userOne);
+//        }
+//    }
 }
