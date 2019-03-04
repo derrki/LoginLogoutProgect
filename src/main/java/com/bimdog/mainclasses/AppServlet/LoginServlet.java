4e465d63923fc5801d20a8ffa4ff7fbea9a1ec8b
Roman Kaminskiy - pc <derrki@gmail.com>
2019-03-05 00:44:19 +0200

    Виконано вибірку даних з двох таблиць. (дуже кривий код !!!!!)

--- src/main/java/com/bimdog/mainclasses/AppServlet/LoginServlet.java
+++ src/main/java/com/bimdog/mainclasses/AppServlet/LoginServlet.java
@@ -12,10 +12,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.io.PrintWriter;
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
+import java.sql.*;
 
 @WebServlet(name = "LoginServlet", urlPatterns = { "/LoginServlet" })
 public class LoginServlet extends HttpServlet {
@@ -53,7 +50,7 @@ public class LoginServlet extends HttpServlet {
             out.println("<font color=red>"+errorMsg+"</font>");
             requestDispatcher.include(request, response);
         }else{
-            String comand = "SELECT id, name, surname, login, password from users WHERE login=? and password=? limit 1";
+            String sqlUsers = "SELECT id, name, surname, login, password from users WHERE login=? and password=? limit 1";
             DatabaseManager databaseManager = new DatabaseManager();
             try {
                 Connection connection = databaseManager.connectDB(HOST_MYSQL, USERNAME_MYSQL, PASSWORD_MYSQL);
@@ -61,15 +58,30 @@ public class LoginServlet extends HttpServlet {
                 e.printStackTrace();
             }
             PreparedStatement preparedStatement = null;
-            ResultSet rs = null;
+            Statement statement = null;
+            ResultSet resultSetUsers = null;
+            ResultSet resultSetCountry = null;
 
             try {
-                preparedStatement = databaseManager.getConnection().prepareStatement(comand);
+                preparedStatement = databaseManager.getConnection().prepareStatement(sqlUsers);
                 preparedStatement.setString(1, login);
                 preparedStatement.setString(2, password);
-                rs = preparedStatement.executeQuery();
-                if(rs != null && rs.next()){
-                    User user = new User(rs.getString("name"), rs.getString("surname"), rs.getString("login"),  rs.getString("password"), null);
+                resultSetUsers = preparedStatement.executeQuery();
+
+
+
+                if(resultSetUsers != null && resultSetUsers.next()){
+
+                    preparedStatement.setString(1, resultSetUsers.getString("id"));
+                    statement = databaseManager.getConnection().createStatement();
+
+                    String sqlCountry = "SELECT id, country from user_country WHERE id=" + resultSetUsers.getString("id");
+                    ResultSet resultSet = databaseManager.query(statement, sqlCountry);
+                    User user = null;
+                    if(resultSet.next()){
+                    String country = resultSet.getString("country");
+                    user = new User(resultSetUsers.getString("name"), resultSetUsers.getString("surname"), resultSetUsers.getString("login"),  resultSetUsers.getString("password"), country);
+                    }
                     HttpSession session = request.getSession();
                     session.setAttribute("User", user);
                     response.sendRedirect("home.jsp");
@@ -80,8 +92,6 @@ public class LoginServlet extends HttpServlet {
                     out.println("<font color=red>No user found with given email id, please register first.</font>");
                     rd.include(request, response);
                 }
-
-
             } catch (SQLException e) {
                 e.printStackTrace();
             }
