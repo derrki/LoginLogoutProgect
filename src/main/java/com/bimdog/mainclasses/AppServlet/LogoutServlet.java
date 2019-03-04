9a360092405632c8be46e681742429294a3f6753
Roman Kaminskiy - pc <derrki@gmail.com>
2019-03-04 13:17:21 +0200

    Реалізовано LogoutServlet.java. Реалізує вихід з реєстрації

--- src/main/java/com/bimdog/mainclasses/AppServlet/LogoutServlet.java
+++ src/main/java/com/bimdog/mainclasses/AppServlet/LogoutServlet.java
@@ -2,27 +2,30 @@ package com.bimdog.mainclasses.AppServlet;
 
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
-import javax.servlet.http.HttpServlet;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
+import javax.servlet.http.*;
 import java.io.IOException;
-import java.io.PrintWriter;
 
-@WebServlet("/LogoutServlet")
+@WebServlet(name = "Logout", urlPatterns = { "/Logout" })
 public class LogoutServlet extends HttpServlet {
-    @Override
-    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+    private static final long serialVersionUID = 1L;
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException, IOException {
         response.setContentType("text/html");
-        PrintWriter out=response.getWriter();
+        Cookie[] cookies = request.getCookies();
+        if(cookies != null){
+            for(Cookie cookie : cookies){
+                if(cookie.getName().equals("JSESSIONID")){
 
-        request.getRequestDispatcher("link.html").include(request, response);
-
-        HttpSession session=request.getSession();
-        session.invalidate();
-
-        out.print("You are successfully logged out!");
-
-        out.close();
+                    break;
+                }
+            }
+        }
+        //invalidate the session if exists
+        HttpSession session = request.getSession(false);
+        if(session != null){
+            session.invalidate();
+        }
+        response.sendRedirect("login.html");
     }
+
 }
